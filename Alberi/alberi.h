80b5f0589f4ac863c01c4db6258e2d0e8166f759
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2023-12-03 08:58:33 +0100

    Commenti modificati

--- Alberi/alberi.h
+++ Alberi/alberi.h
@@ -6,14 +6,12 @@
 
 typedef int TIPO;
 
-//private:
 struct Tree {
     TIPO key;
     struct Tree * left;
     struct Tree * right;
 };
 
-//public:
 struct Tree * NewNode (TIPO k);
 void Print_BFS (struct Tree * T);
 void Print_DFS (struct Tree * T);
--- Alberi/alberiRicerca_Iterativi.h
+++ Alberi/alberiRicerca_Iterativi.h
@@ -3,7 +3,6 @@
 #ifndef ALBERIRICERCA_ITERATIVI_H_
 #define ALBERIRICERCA_ITERATIVI_H_
 
-//public:
 struct Tree * Search_Iterativo (struct Tree * T, TIPO k);
 struct Tree * Min_Iterativo (struct Tree * T);
 struct Tree * Max_Iterativo (struct Tree * T);
