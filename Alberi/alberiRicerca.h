05f3f59c680adb4e38d86da9369578a955afb340
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-01-03 18:58:04 +0100

    commenti modificati

--- Alberi/alberiRicerca.h
+++ Alberi/alberiRicerca.h
@@ -7,9 +7,9 @@ struct Tree * Search (struct Tree * T, TIPO k);
 struct Tree * Min (struct Tree * T);
 struct Tree * Max (struct Tree * T);
 struct Tree * Successore (struct Tree * T, TIPO k);
-struct Tree * Insert (struct Tree * T, TIPO k); //Per la versione ottimizzata sulla memoria, vedere Insert_Iterativo
-struct Tree * Delete (struct Tree * T, TIPO k); //Utile da ricordare per gli alberi bilanciati, dove la Tail Recursion non è possibile
-void DeleteTR (struct Tree * T, TIPO k, struct Tree * Pred); //Delete ottimizzato sulla memoria con Tail Recursion.
+struct Tree * Insert (struct Tree * T, TIPO k);
+struct Tree * Delete (struct Tree * T, TIPO k);
+void DeleteTR (struct Tree * T, TIPO k, struct Tree * Pred); //Esempio di tail recursion.
 
 //funzioni ausiliarie di Delete
 struct Tree * DeleteRoot (struct Tree * T);
