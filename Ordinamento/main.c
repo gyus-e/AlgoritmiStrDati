5310c3806d867212943515d837b8382fcbb31e75
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-02-16 19:37:11 +0100

    main per testare algoritmi ordinamento

---
+++ Ordinamento/main.c
@@ -0,0 +1,67 @@
+#include "array.h"
+#include "stack.h"
+#include "boolean.h"
+
+#include "quicksort.h"
+#include "heapsort.h"
+#include "mergesort.h"
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+
+int main ()
+{
+    unsigned int in = 0;
+    while (true)
+    {
+        printf("Generating new array:\n");
+        int A [N];
+        srand (time(0));
+        fill (A);
+        printArray (A);
+
+        printf ("Press 0 for HeapSort\n"
+        "Press 1 for QuickSort\n"
+        "Press 2 for QuickSort_iterativo\n"
+        "Press 3 for MergeSort\n"
+        "Press 4 for MergeSort_iterativo\n"
+        "Press 5 to quit\n");
+
+        if (scanf ("%u", &in) != 1)
+        {
+            break;
+        }
+
+        switch (in)
+        {
+            case 0:
+            HeapSort (A);
+            break;
+
+            case 1:
+            QuickSort (A, 0, N-1);
+            break;
+
+            case 2:
+            QuickSort_iterativo (A, 0, N-1);
+            break;
+
+            case 3:
+            MergeSort (A, 0, N-1);
+            break;
+
+            case 4:
+            MergeSort_iterativo (A, 0, N-1);
+            break; 
+
+            default:
+            return 0;
+        }
+
+        printf("Sorted array:\n");
+        printArray (A); 
+        printf("\n\n");
+    }
+}
