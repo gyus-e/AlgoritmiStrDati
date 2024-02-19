8f34354bac8e3f439127297761344e2b23e2b37a
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-02-19 15:29:00 +0100

    main ordinamento

--- Ordinamento/main.c
+++ Ordinamento/main.c
@@ -10,28 +10,30 @@
 #include "stdlib.h"
 #include "time.h"
 
-
-#define N 21
-
 int main ()
 {
     unsigned int in = 0;
+    unsigned int dim = 0;
     while (true)
     {
+        printf("Insert size of array:\n");
+        if (scanf ("%u", &dim) != 1 || dim < 1)
+        {
+            break;
+        }
         printf("Generating new array:\n");
-        int A [N];
+        int * A =  malloc (dim * (sizeof(int)));
         srand (time(0));
-        fill (A, N);
-        printArray (A, N);
+        fill (A, dim);
+        printArray (A, dim);
 
         printf ("Press 0 for HeapSort\n"
         "Press 1 for QuickSort\n"
         "Press 2 for QuickSort_iterativo\n"
         "Press 3 for MergeSort\n"
-        "Press 4 for MergeSort_iterativo\n"
-        "Press 5 to quit\n");
+        "Press 4 for MergeSort_iterativo\n");
 
-        if (scanf ("%u", &in) != 1)
+        if (scanf ("%u", &in) != 1 || in > 4)
         {
             break;
         }
@@ -39,31 +41,33 @@ int main ()
         switch (in)
         {
             case 0:
-            HeapSort (A, N);
+            HeapSort (A, dim);
             break;
 
             case 1:
-            QuickSort (A, 0, N-1);
+            QuickSort (A, 0, dim-1);
             break;
 
             case 2:
-            QuickSort_iterativo (A, 0, N-1);
+            QuickSort_iterativo (A, 0, dim-1);
             break;
 
             case 3:
-            MergeSort (A, 0, N-1);
+            MergeSort (A, 0, dim-1);
             break;
 
             case 4:
-            MergeSort_iterativo (A, 0, N-1);
+            MergeSort_iterativo (A, 0, dim-1);
             break; 
 
             default:
-            return 0;
+            break;
         }
 
         printf("Sorted array:\n");
-        printArray (A, N); 
-        printf("\n\n");
+        printArray (A, dim); 
+        free(A);
+        printf("\n"); 
     }
+    return 0;
 }
